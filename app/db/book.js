e3c0f885adcf9d1a79126426e37818e46ae51472
Daniel Aguirre <danter1@gmail.com>
2017-09-22 14:36:10 -0500

    updatet db funciton

--- app/db/book.js
+++ app/db/book.js
@@ -1,19 +1,29 @@
-const bookModel = require('./schema/Book');
+const mongoose = require('mongoose');
+
+const bookModel = mongoose.model('Book');
 
 const Book = {
-  findOne(id) {
-    console.log({find:id})
-    return bookModel.findOne({_id: id}).exec();
-  },
+  findOne: id => bookModel.findOne({ _id: id }).exec(),
+
   find(query) {
+    Object.keys(query)
+    .forEach((key) => {
+      if (query[key] && key !== 'price') {
+        query[key] = new RegExp(`^${query[key]}`, 'i');
+      }
+    });
     return bookModel.find(query).exec();
   },
   save(data) {
-    return bookModel.create(data);
+    console.log('saving');
+    return bookModel.create(data, function(err, data)  {
+      console.log(err, data);
+    });
   },
-  update(id, modifications){
+  update(id, modifications) {
+    console.log('update')
     return bookModel.findByIdAndUpdate(id, { $set: modifications }, { new: true }).exec();
   },
-}
+};
 
 module.exports = Book;
