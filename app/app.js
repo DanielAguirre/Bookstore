5a2ecedf159d2831566a4a580ab561c38a4bd82d
Daniel Aguirre <danter1@gmail.com>
2017-09-22 14:34:27 -0500

    Updated the app setup for show message error when mongodb conection fails

--- app/app.js
+++ app/app.js
@@ -1,13 +1,22 @@
+// import all of our models
+require('./db/schema/book');
 const http = require('http');
 const mongoose = require('mongoose');
 const expressServer = require('./server/expressServer');
 
-mongoose.connect(`mongodb://${process.env.MONGODB_PORT_27017_TCP_ADDR}/books`);
+const PORT = process.env.PORT || 3000;
+// Connect to our Database and handle an bad connections
+
+mongoose.connect(`${process.env.DATABASE_URL}`);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
 
 const server = new http.createServer(expressServer());
 if (!module.parent) {
-  server.listen(3000, () => {
-    console.log('app runining in port 3000');
+  server.listen(process.env.PORT || 3000, () => {
+    console.log(`app runining in port ${PORT}`);
   });
 } else {
   module.exports = server;
